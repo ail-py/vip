1e8931e28aa59192534ce438dd7a42b9cf3f6488
sdcvvvhhyuu <sdcvvvhhyuu@gmail.com>
2025-11-20 11:20:41 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,53 +1,32 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
+# .idx/dev.nix
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  # Select the stable channel
+  channel = "stable-23.11";
+
+  # Critical packages for VLESS + Rust project
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.nodejs_20          # For running JavaScript and Hono
+    pkgs.rustc              # Rust compiler
+    pkgs.cargo              # Rust package manager
+    pkgs.wasm-pack          # Tool to convert Rust to WebAssembly
+    pkgs.nodePackages.pnpm  # Fast package installer
   ];
-  # Sets environment variables in the workspace
-  env = {};
+
+  env = {
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  };
+
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
-      "google.gemini-cli-vscode-ide-companion"
+      "rust-lang.rust-analyzer"
+      "tamasfe.even-better-toml"
+      "esbenp.prettier-vscode"
     ];
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-    # Workspace lifecycle hooks
+
     workspace = {
-      # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ ".idx/dev.nix" "README.md" ];
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        # Automatically install Wrangler (Cloudflare tool)
+        install-wrangler = "npm install -g wrangler";
       };
     };
   };
